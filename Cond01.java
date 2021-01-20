70adfe8127ef03aefde3cd3050acc69ad8de347d
cahenrique <61274761+cahenrique@users.noreply.github.com>
2021-01-20 13:34:44 -0300

    Add files via upload

---
+++ Cond01.java
@@ -0,0 +1,26 @@
+package estrutura_condicional;
+
+import java.util.Scanner;
+
+public class Cond01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner sc =  new Scanner (System.in);
+			int i = sc.nextInt();
+			
+			if (i > -1) {
+				System.out.println("Positivo");
+			} else {
+				System.out.println("Negativo");
+			}
+			
+			
+		
+		
+		
+
+	}
+
+}
